20cb1c41bfe3415444897c5565f77f8ddf932f24
LeeSangHwee <tkdgnl56@naver.com>
2024-01-05 18:00:26 +0900

    20240105

---
+++ Practice/Practice/DynamicArray.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+// 추가
+
+// 검색
+
+// 삭제
+
+int main()
+{
+	int Select;
+	
+	printf("----- 동적배열 -----");
+	printf("1. Add\n");
+	printf("2. Search\n");
+	printf("3. Delete\n");
+	printf("입력 : "), scanf("%d", &Select);
+
+	switch (Select)
+	{
+	case 1:
+
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	default:
+		printf("Error : Select\n");
+		break;
+	}
+
+	return 0;
+}
